720f7ae1a1fd6f40a396534aa03e0e453a89e7aa
Christian Fuchs <chris.fuchs88@gmail.com>
2022-03-17 19:12:36 +0100

    1st Try of UnitTesting (tbc)

--- frontEnd/src/app/components/board-admin/board-admin.component.spec.ts
+++ frontEnd/src/app/components/board-admin/board-admin.component.spec.ts
@@ -1,10 +1,14 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TokenStorageService } from 'src/app/services/token-storage.service';
+import { UserService } from 'src/app/services/user.service';
 
 import { BoardAdminComponent } from './board-admin.component';
 
 describe('BoardAdminComponent', () => {
   let component: BoardAdminComponent;
   let fixture: ComponentFixture<BoardAdminComponent>;
+  let userService: jasmine.SpyObj<UserService>;
+  let tokenStorageService: jasmine.SpyObj<TokenStorageService>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
