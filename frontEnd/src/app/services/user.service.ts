677e29a140b8d6f316a42fa9a3e6fec7f5ad0f04
Christian Fuchs <chris.fuchs88@gmail.com>
2022-04-08 20:42:07 +0200

    remove user+mod+org board routing

--- frontEnd/src/app/services/user.service.ts
+++ frontEnd/src/app/services/user.service.ts
@@ -16,29 +16,17 @@ export class UserService {
   deleteUser(id: any) {
     console.log("delete: ",id);
     return this.http.delete(API_URL + 'delete/' + id, { responseType: 'text' });
-    //return this.http.get(API_URL + 'all', { responseType: 'text' });
   }
   promoteUserToOrganizer(id: any) {
     return this.http.put(API_URL + 'user2org/' + id, { responseType: 'text' });
-    //return this.http.get(API_URL + 'all', { responseType: 'text' });
   }
   promoteUserToModerator(id: any) {
     console.log("promote2mod: ",id);
     return this.http.put(API_URL + 'user2mod/' + id, { responseType: 'text' });
-    //return this.http.get(API_URL + 'all', { responseType: 'text' });
   }
   constructor(private http: HttpClient) { }
   getPublicContent(): Observable<any> {
     return this.http.get(API_URL + 'all', { responseType: 'text' });
-  }
-  getUserBoard(): Observable<any> {
-    return this.http.get(API_URL + 'user', { responseType: 'text' });
-  }
-  getOrganizerBoard(): Observable<any> {
-    return this.http.get(API_URL + 'org');
-  }
-  getModeratorBoard(): Observable<any> {
-    return this.http.get(API_URL + 'mod');
   }
   getAdminBoard(): Observable<any> {
     return this.http.get<User[]>(API_URL + 'admin');
